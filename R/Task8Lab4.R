a9798bc381a6d02d18ddd9190c4a5cc0d44ecfe9
chihyu <chih.yu.chu-1@ou.edu>
2023-11-03 16:30:39 -0500

    initial

---
+++ R/Task8Lab4.R
@@ -0,0 +1,12 @@
+#' Task 8 in lab 4
+#'
+#' @param x A quantitative vector
+#'
+#' @return A vector of squared components
+#' @export
+#'
+#' @examples
+#' Task8Lab4(1:10)
+lab4=function(x){
+  0.86089580 +1.46959217*x  -0.02745726*x^2
+}
